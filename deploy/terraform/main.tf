fe5aa5b91706a541fb2781fc5d5601ce31d2e696
Edson Caliman <edson.caliman.ext@zoop.com.br>
2023-06-21 17:06:16 -0300

    Define terraform files

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,68 @@
+resource "aws_vpc" "customer_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    "Name" = "customerapi_vpc"
+  }
+}
+
+resource "aws_subnet" "customer_subnet" {
+  vpc_id     = aws_vpc.customer_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "customerapi_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "customer_igw" {
+  vpc_id = aws_vpc.customer_vpc.id
+
+  tags = {
+    Name = "customerapi_igw"
+  }
+}
+
+resource "aws_route_table" "customer_rtb" {
+  vpc_id = aws_vpc.customer_vpc.id
+
+  tags = {
+    Name = "customerapi_rtb"
+  }
+}
+
+resource "aws_route" "customer_route" {
+  route_table_id = aws_route_table.customer_rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.customer_igw.id
+}
+
+resource "aws_route_table_association" "customer_rtb_ass" {
+  route_table_id = aws_route_table.customer_rtb.id
+  subnet_id = aws_subnet.customer_subnet.id
+}
+
+resource "aws_instance" "customer_ec2" {
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.customer_key.id
+  vpc_security_group_ids = [aws_security_group.customer_sg.id]
+  subnet_id = aws_subnet.customer_subnet.id
+
+  ami = data.aws_ami.customer_ami.id
+
+  user_data = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  tags = {
+    Name = "customerapi_ec2"
+  }
+}
+
+
---
+++ deploy/terraform/security.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "customer_sg" {
+  name = "customerapi_sg"
+  description = "customer api security group"
+  vpc_id = aws_vpc.customer_vpc.id
+}
+
+resource "aws_security_group_rule" "sgr_pub_out" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.customer_sg.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sgr_ssh_in" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.customer_sg.id
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sgr_https_in" {
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.customer_sg.id
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_key_pair" "customer_key" {
+  key_name = "kotlin-spring"
+  public_key = file("/home/edsonjunior/.ssh/kotlin-spring.pub")
+}
+
