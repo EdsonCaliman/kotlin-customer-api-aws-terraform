f9c957dc091f6ba3a5f0788bda70ac3f9fd554c1
Edson Caliman <edson.caliman.ext@zoop.com.br>
2023-06-21 18:02:09 -0300

    Update terraform files

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ Docker commands:
     docker run -d -p 8080:8080 kotlin-customer-api-aws-terraform
     
 
-Swagger: http://localhost:8080/swagger-ui/index.html
+Swagger: 
+    
+    http://localhost:8080/swagger-ui/index.html
+
+
+Conexão via ssh: 
+    
+    ssh -i /home/edsonjunior/.ssh/kotlin-spring ubuntu@IP
--- deploy/terraform/security.tf
+++ deploy/terraform/security.tf
@@ -22,13 +22,13 @@ resource "aws_security_group_rule" "sgr_ssh_in" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "sgr_https_in" {
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.customer_sg.id
-  to_port           = 443
+resource "aws_security_group_rule" "public_in_http" {
   type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.customer_sg.id
 }
 
 resource "aws_key_pair" "customer_key" {
